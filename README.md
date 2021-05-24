e2f711dbc7ec99b5d52fc148f60a263708875b43
Bruce Johnson <brucejo.wk@outlook.com>
2021-05-24 14:35:10 -0700

    Added dynamic loading of ua-parser-js

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # meteor-browser-version
+
+`meteor-browser-version` is a package used by [`meteor-static-clients`](https://github.com/brucejo75/meteor-static-clients).
+
+`meteor-browser-version` does 2 things:
+1. On the server side if `Meteor.isDevelopment` is true then it outputs a file `private/meteor-static/minBrowserVersions.json` that has the minimum version information.
+2. On the client side, if `__meteor_runtime_config__.minVersions` is defined it calculates whether the client is modern or legacy.  And it sets the cookie `browser-js` to either value of `"modern"` or `"legacy"`.
+
+`meteor-static-clients` looks for the presence of `private/meteor-static/minBrowserVersions.json` if it is available then `meteor-static-clients` loads `minVersions` into `__meteor_runtime_config__` so tht `meteor-browser-version` can determine if the browser is modern or not.
+
+`meteor-browser-version` should be included in any client static builds and it should be included in the hosting meteor server for a static client.
+
+The reason to include in the meteor server is so that the dynamic package loading that `meteor-browser-version` does will be white in the server to support dynamic loading.
--- browser-version_client.js
+++ browser-version_client.js
@@ -1,48 +1,51 @@
-import UAParser from 'ua-parser-js';
 const minVersions = __meteor_runtime_config__.minVersions;
-const expiry = new Date(Date.now() + 1000 * 60 * 60 * 24 * 365);
+
 if(minVersions) {
-  let parser = new UAParser();
-  const browser = parser.getBrowser();
-
-  // Go get the current cookies
-  const cookies = document.cookie.split(';')
-  .reduce((obj, _c) => {
-    let v = _c.trim().split('=');
-    obj[v[0]] = v[1];
-    return obj;
-  }, {})
-
-  browser.name = browser.name.toLocaleLowerCase();
-  browser.versions = browser.version.split('.');
-  browser.js = 'legacy';
-
-  const bMin = minVersions[browser.name];
-  if(bMin) {
-    // bMin is either the major number or an array of numbers
-    if(typeof bMin === 'number' && browser.versions[0] >= bMin) browser.js = 'modern';
-    else {
-      browser.js = 'modern';
-      for(let i = 0; i < bMin.length; i++) {
-        // All versions up to now have been equal to
-        if(browser.versions[i] > bMin[i]) break;
-        if(browser.versions[i] < bMin[i]) {
-          browser.js = 'legacy';
-          break;
+  Meteor.startup(async function browserVersionStartup() {
+    const {UAParser} = await import('ua-parser-js');
+    let parser = new UAParser();
+    const browser = parser.getBrowser();
+  
+    // Go get the current cookies
+    const cookies = document.cookie.split(';')
+    .reduce((obj, _c) => {
+      let v = _c.trim().split('=');
+      obj[v[0]] = v[1];
+      return obj;
+    }, {})
+  
+    browser.name = browser.name.toLocaleLowerCase();
+    browser.versions = browser.version.split('.');
+    browser.js = 'legacy';
+  
+    const bMin = minVersions[browser.name];
+    if(bMin) {
+      // bMin is either the major number or an array of numbers
+      if(typeof bMin === 'number' && browser.versions[0] >= bMin) browser.js = 'modern';
+      else {
+        browser.js = 'modern';
+        for(let i = 0; i < bMin.length; i++) {
+          // All versions up to now have been equal to
+          if(browser.versions[i] > bMin[i]) break;
+          if(browser.versions[i] < bMin[i]) {
+            browser.js = 'legacy';
+            break;
+          }
         }
       }
     }
-  }
-
-  // cookie expires in 1 yr
-  // always set it with new expiration
-  document.cookie=`browser-js=${browser.js};same-site=strict;expires=${expiry.toUTCString()}`
-
-  // The value updated, reload
-  if(!cookies['browser-js'] || cookies['browser-js'] !== browser.js) {
-    location.reload();
-  }
+  
+    // cookie expires in 1 yr
+    // always set it with new expiration
+    const expiry = new Date(Date.now() + 1000 * 60 * 60 * 24 * 365);
+    document.cookie=`browser-js=${browser.js};same-site=strict;expires=${expiry.toUTCString()}`
+  
+    // The value updated, reload
+    if(!cookies['browser-js'] || cookies['browser-js'] !== browser.js) {
+      location.reload();
+    }
+  });
 } else {
   // make sure there is no cookie remnant
-  document.cookie = "browser-js=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+  document.cookie = "browser-js=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
 }

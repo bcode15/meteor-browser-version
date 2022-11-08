1cf5669c1629bfa2fcea6a6539531b26c4913807
Bruce Johnson <brucejo.wk@outlook.com>
2022-11-08 15:34:09 -0800

    bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'bcode15:browser-version',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Set browser version cookies',
   // URL to the Git repository containing the source code for this package.
